521a35f24ff59d3f63a5685885a09b8fa7bb0d4c
silent-sea1119 <sandragonzales56t@gmail.com>
2018-11-12 15:26:27 +0530

    HTTP Methods in react

--- src/reactify-ui/src/App.js
+++ src/reactify-ui/src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+import Posts from './posts/Posts'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Posts />
+    )
   }
 }
 
-export default App;
+export default App
