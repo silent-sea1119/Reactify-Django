5bb38873a62a23a2d5e97bdb059810e7c141bc65
silent-sea1119 <sandragonzales56t@gmail.com>
2018-11-13 20:44:18 +0530

    How react renders

--- src/reactify-ui/src/index.js
+++ src/reactify-ui/src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+// import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+let myComponent = document.getElementById('reactify-django-ui')
+if (myComponent !== null) {
+  ReactDOM.render(<App />, myComponent)
+}
+
+// Here we can render any number of components we want
+// For example
+//
+// let myComponent2 = document.getElementById('reactify-django-ui-2')
+// if (myComponent2 !== null) {
+//   ReactDOM.render(<App />, myComponent2)
+// }
+
+// registerServiceWorker()
