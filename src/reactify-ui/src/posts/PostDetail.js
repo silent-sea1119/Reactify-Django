dbd2366efc2b13a0e2c852bc4c4a02823aa26cb5
silent-sea1119 <sandragonzales56t@gmail.com>
2018-11-21 20:52:11 +0530

    Post Detail View

--- src/reactify-ui/src/posts/PostDetail.js
+++ src/reactify-ui/src/posts/PostDetail.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import 'whatwg-fetch'
 
 class PostDetail extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      slug: null
+      slug: null,
+      post: null,
+      doneLoading: false
     }
   }
+
+  loadPost (slug) {
+    const endpoint = `/api/posts/${slug}/`
+    let thisComponent = this
+    let lookupOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    fetch(endpoint, lookupOptions)
+      .then(function (response) {
+        if (response.status === 404) {
+          console.log('Page not found')
+        }
+        return response.json()
+      }).then(function (responseData) {
+        console.log(responseData)
+        if (responseData.detail) {
+          thisComponent.setState({
+            post: null,
+            doneLoading: true
+          })
+        } else {
+          thisComponent.setState({
+            post: responseData,
+            doneLoading: true
+          })
+        }
+      }).catch(function (error) {
+        console.log('error', error)
+      })
+  }
+
   componentDidMount () {
     if (this.props.match) {
       // A match object contains information about how a <Route path> matched the URL
@@ -15,19 +53,24 @@ class PostDetail extends Component {
       this.setState({
         slug: slug
       })
+      this.loadPost(slug)
     }
   }
 
   render () {
-    const { slug } = this.state
+    const { doneLoading } = this.state
+    const { post } = this.state
     return (
-      <p>{slug !== null ? <div>
-        {slug}
-        <p className='lead'><Link maintainScrollPosition={false} to={{
-          pathname: `/posts`,
-          state: { fromDashboard: false }
-        }}>Posts</Link></p>
-      </div> : 'Not found'}</p>
+      <p>{doneLoading === true ? <div>
+        {post === null ? 'Not Found' : <div>
+          <h1>{post.title}</h1>
+          {post.slug}
+          <p className='lead'><Link maintainScrollPosition={false} to={{
+            pathname: `/posts`,
+            state: { fromDashboard: false }
+          }}>Posts</Link></p>
+        </div>}
+      </div> : 'Loading...'}</p>
     )
   }
 }
